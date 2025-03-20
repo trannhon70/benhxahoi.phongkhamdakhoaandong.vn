0b0cd5e55862f4556d76165504cfc6216d79c9f7
tran nhon <trannhon@trans-MacBook-Air.local>
2025-03-20 16:34:33 +0700

    update

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -9,8 +9,8 @@ if (isset($_GET['action']) && $_GET['action'] == 'logout') {
     Session::destroy();
 }
 
-// $local = 'http://localhost/_andong/benhxahoi.phongkhamdakhoaandong.vn';
-$local = 'https://benhxahoi.phongkhamdakhoaandong.vn';
+$local = 'http://localhost/_andong/benhxahoi.phongkhamdakhoaandong.vn';
+// $local = 'https://benhxahoi.phongkhamdakhoaandong.vn';
 ?>
 <?php
 function getImagesFromFolder($folderPath) {
--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
-// define("DB_HOST", "localhost");
-// define("DB_USER", "root");
-// define("DB_PASS", "");
-// define("DB_NAME", "phongkhamandong.vn");
-
-
 define("DB_HOST", "localhost");
-define("DB_USER", "bxh_pkdkandong_vn");
-define("DB_PASS", "nXraThMaRzBPEBYt");
-define("DB_NAME", "bxh_pkdkandong_vn");   
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "phongkhamandong.vn");
+
+
+// define("DB_HOST", "localhost");
+// define("DB_USER", "bxh_pkdkandong_vn");
+// define("DB_PASS", "nXraThMaRzBPEBYt");
+// define("DB_NAME", "bxh_pkdkandong_vn");   
 ?>
